bc03bd60133191f0eb15756585ae0f462131b694
lauraolivaresbenitez <olivareslaura027@gmai.com>
2024-02-17 00:37:28 +0100

    updated

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -32,7 +32,7 @@ def extractConcepts(prompt: str, metadata={}, model="mistral-openorca:latest"):
     return result
 
 
-def graphPrompt(input: str, metadata={}, model="mistral-openorca:latest"):
+def graphPrompt(input: str, metadata={}, model=None):
     if model == None:
         model = "mistral-openorca:latest"
 
@@ -40,26 +40,49 @@ def graphPrompt(input: str, metadata={}, model="mistral-openorca:latest"):
     # print( chalk.blue(model_info))
 
     SYS_PROMPT = (
-        "You are a network graph maker who extracts terms and their relations from a given context. "
-        "You are provided with a context chunk (delimited by ```) Your task is to extract the ontology "
-        "of terms mentioned in the given context. These terms should represent the key concepts as per the context. \n"
-        "Thought 1: While traversing through each sentence, Think about the key terms mentioned in it.\n"
-            "\tTerms may include object, entity, location, organization, person, \n"
-            "\tcondition, acronym, documents, service, concept, etc.\n"
-            "\tTerms should be as atomistic as possible\n\n"
-        "Thought 2: Think about how these terms can have one on one relation with other terms.\n"
-            "\tTerms that are mentioned in the same sentence or the same paragraph are typically related to each other.\n"
-            "\tTerms can be related to many other terms\n\n"
-        "Thought 3: Find out the relation between each such related pair of terms. \n\n"
-        "Format your output as a list of json. Each element of the list contains a pair of terms"
-        "and the relation between them, like the follwing: \n"
+        "You are a pharmaceutical knowledge graph creator tasked with extracting pharmacological interactions from articles. This articles are chunked."
+        "You will be provided with a context chunk, delimited by triple backticks (```). Your goal is to identify the ontology of pharmacs and their interactions."
+        "Consider the following thoughts while processing the context:\n"
+        "Thought 1: While analyzing each sentence, focus on key terms related to pharmacological interactions. Consider terms such as 'Drugs', 'pharmacokinetics (PK)', 'pharmacodynamics (PD)', 'pharmacogenetics (PG)', diseases.\n"
+        "\tTry to break down terms into atomic components.\n\n"
+        "Thought 2: Explore how these drugs interacts between them with expresions such as the interaction of drug and drug, interacts with,potential interaction between, can affect the metabolism of, enhances the effects of, may potentiate the effects of.\n"
+        "\tTerms mentioned in the same sentence or paragraph are likely related.\n"
+        "\tTerms can have multiple relationships with other terms.\n\n"
+        "Thought 3: Determine the interactions between pairs of terms.\n\n"
+        "When extracting properties such as 'Type of Interaction,' 'Severity,' 'Gender,' and 'Pregnancy,' pay attention to expressions like:\n"
+        "- 'Type of Interaction': Look for phrases indicating the nature of the interaction, e.g., 'synergistic,' 'antagonistic,' 'additive.'\n"
+        "- 'Severity': Identify words conveying the intensity or seriousness of effects, e.g., 'severe,' 'mild,' 'moderate.'\n"
+        "- 'Gender': Seek terms indicating the gender specificity of interactions, e.g., 'male,' 'female.'\n"
+        "- 'Pregnancy': Recognize language discussing the impact of interactions on pregnancy, e.g., 'safe for pregnant individuals,' 'contraindicated during pregnancy.'\n\n"
+        "Format your output as a list of JSON objects. Each element in the list should represent a pair of terms and the interaction between them. For example:\n"
         "[\n"
-        "   {\n"
-        '       "node_1": "A concept from extracted ontology",\n'
-        '       "node_2": "A related concept from extracted ontology",\n'
-        '       "edge": "relationship between the two concepts, node_1 and node_2 in one or two sentences"\n'
-        "   }, {...}\n"
-        "]"
+    "   {\n"
+    '       "node_1": "Drug",\n'
+    '       "node_2": "Drug",\n'
+    '       "edge": "HAS_INTERACTION",\n'
+    '       "properties": {\n'
+    '           "type": "Synergistic",\n'
+    '           "severity": "High",\n'
+    '           "gender": "Differential Dosages for Males and Females",\n'
+    '           "pregnancy": Safe for use during pregnancy,\n'
+    '           "contraindications": "History of Heart Disease",\n'
+    '           "adverse_effects": "Gastrointestinal Irritation"\n'
+    '       }\n'
+    "   },\n"
+    "   {\n"
+    '       "node_1": "Drug",\n'
+    '       "node_2": "Disease",\n'
+    '       "edge": "HAS_INTERACTION",\n'
+    '       "properties": {\n'
+    '           "type": "Synergistic",\n'
+    '           "severity": "High",\n'
+    '           "gender": "Differential Dosages for Males and Females",\n'
+    '           "pregnancy": Safe for use during pregnancy,\n'
+    '           "contraindications": "History of Heart Disease",\n'
+    '           "adverse_effects": "Gastrointestinal Irritation"\n'
+    '       }\n'
+    "   }\n"
+    "]"
     )
 
     USER_PROMPT = f"context: ```{input}``` \n\n output: "
